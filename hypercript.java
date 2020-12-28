929111224327a0bc067d8338adcb9bd0a31c372a
James D, Hogan III <40651797+RealSuperMiWare@users.noreply.github.com>
2020-12-28 02:59:09 -0600

    New method
    This update moves the code from the main method and places into new encrypt method and adds params such that the user can change the encryption pass values easily. In preparation for a full make over that's coming soon.

--- hypercript.java
+++ hypercript.java
@@ -3,83 +3,21 @@ import java.util.Scanner;
 public class hypercript{
     
     public static void main(String[] args){
-        /* count number of iterations in forloop */
-        int count_iteration = 0;
+        /**I will create 3 scanners that, for now, will be a test to take the user 
+         * dictated encryption passes
+         * 
+         * 
+         * This code will be implemented on the next update
+        */
 
-        /** Create a scanner to catch user string input */
-        Scanner string_to_encrypt = new Scanner(System.in);
-
-        /*Catch scanner input and convert to string*/
-        System.out.println(">>> : ");
-        String caughtString = string_to_encrypt.nextLine();
-
-        /** cast string to char array */
-        char[] string_to_char = caughtString.toCharArray();
-
-        /** create string to hold encrypted value */
-        String encryptedString = "";
-
-        /** loop through array and encrypt values */
-        for(char c : string_to_char){
-            /*count each iteration*/
-            count_iteration++;
-
-            /*each iteration needs to add a value to the ascii unit
-            in this case the value is 5*/
-            c += 5;
-
-            if(Character.isUpperCase(c)){
-                Character.toLowerCase(c);
-                System.out.println("To lower case");
-            }
-
-            /**Every 3rd iteration, add 3 to the ascii unit*/
-            if(count_iteration%3 == 0){
-                c += 2;
-            }
-
-            /**Every 4th iteration, subtract 3 from the ascii unit*/
-            if(count_iteration % 4 == 0){
-                c -= 3;
-            }
-            
-            /**Every 5th iteration, subtract -666 from the ascii unit*/
-            if(count_iteration % 5 == 0){
-                c -= 666;
-            }
-
-            encryptedString += c;
-
-            /*lastly print the encrypted text*/
-            System.out.print(c);
-        }
-
-        /*Now that the first encryption is done*/
-        /*restart loop count*/
-        count_iteration = 0;
-
-        System.out.println("\nLength Reported : " + string_to_char.length);
-        final int mid1 = encryptedString.length() / 2; //get the middle of the String
-
-        /** call returnbool to check condition if true carry out code. it will always be true because the data
-         * will always be split in half and the 2 seperations will be set : the second half will become the first, 
-         * and the first half will become the second half. 
-         */
-        if(returnbool(caughtString)){
-            String[] parts = {encryptedString.substring(0, mid1), encryptedString.substring(mid1)};
-            System.out.println("First seperation : " + parts[1]); //first part
-            System.out.println("Second Seperation : " + parts[0]); //second part
-            System.out.println(returnbool("String : " + caughtString));
-
-            /** this loop will encrypt the new seperate sections of the data, possibly adding new bits
-             * and seperating them again
-             */
-            for(char c : string_to_char){
-            }
-        }
-
-        /*Stop any data leak and close the scanner to preserve memeory*/
-        string_to_encrypt.close();
+        /**The 3 int init below will allow the user to dictate their
+         * needs for the encrypt method
+         * These static values will be changed to allow input by the user
+        */
+        int pass1 = 2;
+        int pass2 = 3;
+        int pass3 = 666;
+        encrypt(pass1, pass2, pass3);
     }
 
     /** This boolean will report if a seperation was passed or failed
@@ -94,5 +32,81 @@ public class hypercript{
             System.out.println("Speration 2 : Failed ");
             return false;
         }
+    }
+
+    private static void encrypt (int pass1, int pass2, int pass3){
+                /* count number of iterations in forloop */
+                int count_iteration = 0;
+
+                /** Create a scanner to catch user string input */
+                Scanner string_to_encrypt = new Scanner(System.in);
+        
+                /*Catch scanner input and convert to string*/
+                System.out.println(">>> Data to ecnrypt : ");
+                String caughtString = string_to_encrypt.nextLine();
+        
+                /** cast string to char array */
+                char[] string_to_char = caughtString.toCharArray();
+        
+                /** create string to hold encrypted value */
+                String encryptedString = "";
+        
+                /** loop through array and encrypt values */
+                for(char c : string_to_char){
+                    /*count each iteration*/
+                    count_iteration++;
+        
+                    /*each iteration needs to add a value to the ascii unit
+                    in this case the value is 5*/
+                    c += 5;
+        
+                    /**Every 3rd iteration, add 3 to the ascii unit*/
+                    if(count_iteration%3 == 0){
+                        c += pass1;
+                    }
+        
+                    /**Every 4th iteration, subtract 3 from the ascii unit*/
+                    if(count_iteration % 4 == 0){
+                        c -= pass2;
+                    }
+                    
+                    /**Every 5th iteration, subtract -666 from the ascii unit*/
+                    if(count_iteration % 5 == 0){
+                        c -= pass3;
+                    }
+        
+                    encryptedString += c;
+        
+                    /*lastly print the encrypted text*/
+                    System.out.print(c);
+                }
+        
+                /*Now that the first encryption is done*/
+                /*restart loop count*/
+                count_iteration = 0;
+        
+                System.out.println("\nLength Reported : " + string_to_char.length);
+                final int mid1 = encryptedString.length() / 2; //get the middle of the String
+        
+                /** call returnbool to check condition if true carry out code. it will always be true because the data
+                 * will always be split in half and the 2 seperations will be set : the second half will become the first, 
+                 * and the first half will become the second half. 
+                 */
+                if(returnbool(caughtString)){
+                    String[] parts = {encryptedString.substring(0, mid1), encryptedString.substring(mid1)};
+                    System.out.println("First seperation : " + parts[1]); //first part
+                    System.out.println("Second Seperation : " + parts[0]); //second part
+                    System.out.println(returnbool("String : " + caughtString));
+        
+                    /** this loop will encrypt the new seperate sections of the data, possibly adding new bits
+                     * and seperating them again
+                     */
+                    for(char c : string_to_char){
+                        /*This may not yet need to be used... I'll keep it on the back burner because I may not use it*/
+                    }
+                }
+        
+                /*Stop any data leak and close the scanner to preserve memory*/
+                string_to_encrypt.close();
     }
 }
